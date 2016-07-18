e011e814fac2bf5ba0f09276c66801475b69c43b
frog <yz405019188@163.com>
2016-07-18 16:36:08 +0800

    去掉s

--- weixin.config.php
+++ weixin.config.php
@@ -1,5 +1,5 @@
 <?php
 define('APPID','wx0b22d045cb332b59');
-define('APPSECRET','a65d20787c838603d5bf86daaec72f7ds');
+define('APPSECRET','a65d20787c838603d5bf86daaec72f7d');
 define('TOKEN','jiyouce');
 ?>
