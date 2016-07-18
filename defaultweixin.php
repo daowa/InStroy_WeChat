535bf82f246626aa07c85f232cf1afa68c407714
frog <yz405019188@163.com>
2016-07-18 14:06:26 +0800

    test

--- defaultweixin.php
+++ defaultweixin.php
@@ -51,7 +51,7 @@ class DefaultWeixin extends wxmessage {
      * return news
      */
     private function fulinews() {
-        $text = 'QQ黄钻、蓝钻、红钻、绿钻或10Q币任选其一';
+        $text = 'QQ榛勯捇銆佽摑閽汇�佺孩閽汇�佺豢閽绘垨10Q甯佷换閫夊叾涓�';
         $posts = array(
             array(
                 'title' => '福利来了',
@@ -78,7 +78,7 @@ class DefaultWeixin extends wxmessage {
      * return jokes
      */
     private function xiaohua() {
-        $text = "你好，亲爱的朋友，我可能不在电脑旁。先看个笑话吧。有个小姑娘穿了一件白色大衣在等车，一个熊孩子把巧克力雪糕整个拍她身上了，孩子他妈说对不起孩子很皮，姑娘蹲下身和蔼的说：小朋友，我们拉钩，以后谁在大马路上瞎闹谁就死全家好不好？孩子他妈吓尿了~";
+        $text = "浣犲ソ锛屼翰鐖辩殑鏈嬪弸锛屾垜鍙兘涓嶅湪鐢佃剳鏃併�傚厛鐪嬩釜绗戣瘽鍚с�傛湁涓皬濮戝绌夸簡涓�浠剁櫧鑹插ぇ琛ｅ湪绛夎溅锛屼竴涓唺瀛╁瓙鎶婂阀鍏嬪姏闆硶鏁翠釜鎷嶅ス韬笂浜嗭紝瀛╁瓙浠栧璇村涓嶈捣瀛╁瓙寰堢毊锛屽濞樿共涓嬭韩鍜岃敿鐨勮锛氬皬鏈嬪弸锛屾垜浠媺閽╋紝浠ュ悗璋佸湪澶ч┈璺笂鐬庨椆璋佸氨姝诲叏瀹跺ソ涓嶅ソ锛熷瀛愪粬濡堝悡灏夸簡~";
         $xml = $this->outputText($text);
         header('Content-Type: application/xml');
         echo $xml;
@@ -88,8 +88,8 @@ class DefaultWeixin extends wxmessage {
      * return welcome msg
      */
     private function welcome() {
-        $text = "亲爱的朋友，欢迎关注兔子。回复“news”看看兔子的10元Q币小礼吧。";
-        // outputText 用来返回文本信息
+        $text = "浜茬埍鐨勬湅鍙嬶紝娆㈣繋鍏虫敞鍏斿瓙銆傚洖澶嶁�渘ews鈥濈湅鐪嬪厰瀛愮殑10鍏僎甯佸皬绀煎惂銆�";
+        // outputText 鐢ㄦ潵杩斿洖鏂囨湰淇℃伅
         $xml = $this->outputText($text);
         header('Content-Type: application/xml');
         echo $xml;
@@ -97,8 +97,8 @@ class DefaultWeixin extends wxmessage {
 
     private function music() {
         $music = array(
-            'title' => '在春天里',
-            'discription' => '在春天里-汪峰',
+            'title' => '鍦ㄦ槬澶╅噷',
+            'discription' => '鍦ㄦ槬澶╅噷-姹嘲',
             'musicurl' => 'http://rubyeye-rubyeye.stor.sinaapp.com/inspring.wma',
             'hdmusicurl' => 'http://rubyeye-rubyeye.stor.sinaapp.com/inspring.mp3'
         );
@@ -110,8 +110,8 @@ class DefaultWeixin extends wxmessage {
 
     private function yishengmusic() {
         $music = array(
-            'title' => '一生所爱',
-            'discription' => '为什么选这首歌呢？因为我的梦想是与一生所爱的人快乐一生。你的呢，亲爱的朋友？',
+            'title' => '涓�鐢熸墍鐖�',
+            'discription' => '涓轰粈涔堥�夎繖棣栨瓕鍛紵鍥犱负鎴戠殑姊︽兂鏄笌涓�鐢熸墍鐖辩殑浜哄揩涔愪竴鐢熴�備綘鐨勫憿锛屼翰鐖辩殑鏈嬪弸锛�',
             'musicurl' => 'http://rubyeye-rubyeye.stor.sinaapp.com/song/%E5%8D%A2%E5%86%A0%E5%BB%B7-%E4%B8%80%E7%94%9F%E6%89%80%E7%88%B1.mp3',
             'hdmusicurl' => 'http://rubyeye-rubyeye.stor.sinaapp.com/song/%E5%8D%A2%E5%86%A0%E5%BB%B7-%E4%B8%80%E7%94%9F%E6%89%80%E7%88%B1.mp3'
         );
@@ -121,7 +121,7 @@ class DefaultWeixin extends wxmessage {
     }
 
     /**
-     * Pre processing，common usage:save the request into your database.
+     * Pre processing锛宑ommon usage:save the request into your database.
 	 * Because weixin save your msgs only 5 days.
      * @return boolean
      */
