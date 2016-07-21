cc95f95eb14c35d72a11669305a0c51cee8fd887
frog <yz405019188@163.com>
2016-07-21 11:34:52 +0800

    insert

--- opeartor/test.php
+++ opeartor/test.php
@@ -7,10 +7,10 @@ $SAE_MYSQL_PORT = "3307";
 $SAE_MYSQL_DB = "app_instory";
  
 function test(){
-	echo "alert(\"开始连接数据库\");";
 	// 连主库
-	$db = mysqli_connect(SAE_MYSQL_HOST_M,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB,SAE_MYSQL_PORT);
-	echo "alert(\"开始连接数据库2\");";
+	$mysql = mysqli_connect(SAE_MYSQL_HOST_M,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB,SAE_MYSQL_PORT);
+	$query = "insert into test('content') values 'hi'";
+	$result = $mysql->query($query);
 	
 	// 连从库
 	// $db = mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
