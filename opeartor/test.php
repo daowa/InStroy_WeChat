d0aa378e58a9f367c6d52c51ad8520d2cff14c46
frog <yz405019188@163.com>
2016-07-21 10:56:41 +0800

    g

--- opeartor/test.php
+++ opeartor/test.php
@@ -7,10 +7,10 @@ $SAE_MYSQL_PORT = 3307;
 $SAE_MYSQL_DB = "app_instory";
  
 function test(){
-	echo "alert(\"开始连接数据库\")</br>";
+	echo "alert(\"开始连接数据库\");";
 	// 连主库
-	$db = mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
-	echo "alert(\"开始连接数据库2\")</br>";
+	mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS,SAE_MYSQL_DB);
+	echo "alert(\"开始连接数据库2\");";
 	
 	// 连从库
 	// $db = mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
