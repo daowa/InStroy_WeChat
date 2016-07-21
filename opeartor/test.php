8958a23d8a1137781d1d8857f98f0300c547417c
frog <yz405019188@163.com>
2016-07-21 10:47:43 +0800

    a

--- opeartor/test.php
+++ opeartor/test.php
@@ -15,10 +15,10 @@ function test(){
 	// 连从库
 	// $db = mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
 	 
-	if ($db) {
-		mysql_select_db(SAE_MYSQL_DB, $db);
-		echo "alert(\"连接从库完成\")";
-	}
+// 	if ($db) {
+// 		mysql_select_db(SAE_MYSQL_DB, $db);
+// 		echo "alert(\"连接从库完成\")";
+// 	}
 }
 ?>
 
