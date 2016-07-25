7e84f71a457d8400200fd9af970646fa4900ec6f
frog <yz405019188@163.com>
2016-07-25 16:06:52 +0800

    导游介绍

--- web/Map/Story.php
+++ web/Map/Story.php
@@ -13,7 +13,7 @@
 
 <body>
 <div class="divImg">
-	<img src="http://pic.lvmama.com/uploads/pc/place2/2014-10-22/1413950255839.jpg">
+	<img src="http://img5.imgtn.bdimg.com/it/u=519206360,3740008041&fm=21&gp=0.jpg">
 </div>
 <div class="content">
 	<div class="voice"></div>
