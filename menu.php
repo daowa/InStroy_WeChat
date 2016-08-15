71f2d43c6da83181ffb0e23aa2c55e9a6624b71a
frog <yz405019188@163.com>
2016-08-15 14:07:47 +0800

    修改了目录结构，目前是 集邮册3.1

--- menu.php
+++ menu.php
@@ -7,75 +7,36 @@ $ACCESS_TOKEN=$ret['access_token'];
 $menuPostData='{
 "button":[
 {
-"name":"自助游玩",
+"name":"附近",
 "sub_button":[
 	{
-	"type":"click",
-	"name":"附近景点",
-	"key":"V142857_NearSpot"
-	},
-	{
-	"type":"click",
-	"name":"附近故事",
-	"key":"V142857_NearStory"
-	},
 	{
 	"type":"view",
-	"name":"景区地图",
+	"name":"地图",
 	"url":"http://instory.applinzi.com/web/Map/Map.php"
 	},
 	{
 	"type":"view",
-	"name":"语音导游",
+	"name":"景点",
 	"url":"http://instory.applinzi.com/web/SelfGuide/SelfGuide.php"
 	},
-	{
-	"type":"click",
-	"name":"旅游线路",
-	"key":"V142857_RecommendLine"
-	}]
 },
 {
-"name":"景区服务",
-"sub_button":[
-	{
-	"type":"click",
-	"name":"景区公告",
-	"key":"V142857_Announcement"
-	},
-	{
-	"type":"view",
-	"name":"景区客服",
-	"url":"http://instory.applinzi.com/web/SpotList/StoryDetail.php"
-	},
-	{
-	"type":"view",
-	"name":"真人导游",
-	"url":"http://instory.applinzi.com/web/TourGuide/TourGuide.php"
-	},
-	{
-	"type":"view",
-	"name":"更换景区",
-	"url":"http://instory.applinzi.com/web/ChangeSpot/ChangeSpot.php"
-	}]
+"name":导游",
+"url":"http://instory.applinzi.com/web/SelfGuide/SelfGuide.php"
 },
 {
-"name":"景点互动",
+"name":"集游册",
 "sub_button":[
 	{
 	"type":"view",
-	"name":"最美照片",
-	"url":"http://instory.applinzi.com/web/Picture/Picture.php"
+	"name":"集游册",
+	"url":"http://instory.applinzi.com/web/Album/Album.php"
 	},
 	{
 	"type":"view",
-	"name":"我的足迹",
+	"name":"游记",
 	"url":"http://instory.applinzi.com/web/TravelNotes/TravelNotes.php"
-	},
-	{
-	"type":"view",
-	"name":"个人中心",
-	"url":"http://instory.applinzi.com/web/PersonalCenter/PersonalCenter.php"
 	}]
 }]
 }';
